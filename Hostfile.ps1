0162bd997438a93874400c2157d5dd8ade8e3407
highaltidude <67246325+highaltidude@users.noreply.github.com>
2021-03-05 15:34:13 -0500

    Initial File

---
+++ Hostfile.ps1
@@ -0,0 +1,48 @@
+#For use within Chick Fil A Environment Only
+#
+#Powershell script to change host file to those needed for Manitou
+
+function setHostEntries([hashtable] $entries) {
+    $hostsFile = "$env:windir\System32\drivers\etc\hosts"
+    $newLines = @()
+
+    $c = Get-Content -Path $hostsFile
+    foreach ($line in $c) {
+        $bits = [regex]::Split($line, "\s+")
+        if ($bits.count -eq 2) {
+            $match = $NULL
+            ForEach($entry in $entries.GetEnumerator()) {
+                if($bits[1] -eq $entry.Key) {
+                    $newLines += ($entry.Value + '     ' + $entry.Key)
+                    Write-Host Replacing HOSTS entry for $entry.Key
+                    $match = $entry.Key
+                    break
+                }
+            }
+            if($match -eq $NULL) {
+                $newLines += $line
+            } else {
+                $entries.Remove($match)
+            }
+        } else {
+            $newLines += $line
+        }
+    }
+
+    foreach($entry in $entries.GetEnumerator()) {
+        Write-Host Adding HOSTS entry for $entry.Key
+        $newLines += $entry.Value + '     ' + $entry.Key
+    }
+
+    Write-Host Saving $hostsFile
+    Clear-Content $hostsFile
+    foreach ($line in $newLines) {
+        $line | Out-File -encoding ASCII -append $hostsFile
+    }
+}
+
+$entries = @{
+    'la-man-review' = "10.2.10.231"
+    'la-man-review2' = "10.2.10.231"
+};
+setHostEntries($entries)
